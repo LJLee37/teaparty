6c8c8129536e8e30b5c269244a79ce5dda8492f3
LJLee37 <ljlee3759@gmail.com>
2024-09-10 14:52:50 +0900

    Init

---
+++ src/i18n/index.js
@@ -0,0 +1,11 @@
+import enUS from "./en-US";
+import koKR from "./ko-KR";
+import enGB from "./en-GB";
+
+export default {
+  "en-US": enUS,
+  "ko-KR": koKR,
+  "en-GB": enGB,
+  en: enGB,
+  ko: koKR,
+};
---
+++ src/pages/ErrorNotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="fullscreen bg-orange-8 text-white text-center q-pa-md flex flex-center"
+  >
+    <div>
+      <div style="font-size: 30vh">404</div>
+
+      <div class="text-h2" style="opacity: 0.4">
+        There's no such tea here...
+      </div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="white"
+        text-color="primary"
+        unelevated
+        to="/"
+        label="Go Home"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({
+  name: "ErrorNotFound",
+});
+</script>
