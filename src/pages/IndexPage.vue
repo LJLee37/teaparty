28c5a950a1d27e758804593625ebd3363d42276c
LJLee37 <ljlee3759@gmail.com>
2024-09-20 16:36:18 +0900

    Add review form in NewReview.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,12 @@
 <template>
   <q-page class="flex flex-center">
     <!--grid or listed reviews of teas-->
-    <q-btn color="primary" icon="add_circle" label="Add" to="/newReview" />
+    <q-btn
+      color="primary"
+      icon="add_circle"
+      label="Add New Review"
+      to="/newReview"
+    />
   </q-page>
 </template>
 
--- src/pages/NewReview.vue
+++ src/pages/NewReview.vue
@@ -1,20 +1,41 @@
 <template>
   <q-page class="flex flex-center">
-    <q-btn
-      label="Add New Review"
-      color="primary"
-      icon="add_circle"
-      @click="add_onclick"
-    ></q-btn>
+    <div>
+      <div class="row q-py-sm">
+        <q-input class="col-10" v-model="teaCompany" label="Tea Company" />
+        <q-input class="col-10" v-model="teaName" label="Tea Name" />
+        <q-input
+          class="col-10"
+          v-model="temperature"
+          label="Temperature(in Celsius)"
+          type="number"
+        />
+        <q-input
+          class="col-10"
+          v-model="brewTime"
+          label="Brew Time(in seconds)"
+          type="number"
+        />
+      </div>
+      <div>
+        <q-btn
+          label="Add"
+          color="primary"
+          icon="add_circle"
+          @click="add_onclick"
+          class="col-6"
+        />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from "vue";
-const teaCompany = ref("Twinings");
-const teaName = ref("Cylon");
-const temperature = ref(80);
-const brewTime = ref(60 * 3 + 30);
+const teaCompany = ref("");
+const teaName = ref("");
+const temperature = ref(100);
+const brewTime = ref(120);
 const add_onclick = () => {
   console.log("Add new review");
   console.log("Tea Company: ", teaCompany.value);
