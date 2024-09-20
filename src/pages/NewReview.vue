f99007690420a4b38593cb31d22b53f1353252bc
LJLee37 <ljlee3759@gmail.com>
2024-09-20 18:54:58 +0900

    Edit review form in NewReview.vue

--- src/pages/NewReview.vue
+++ src/pages/NewReview.vue
@@ -1,29 +1,38 @@
 <template>
   <q-page class="flex flex-center">
     <div>
-      <div class="row q-py-sm">
-        <q-input class="col-10" v-model="teaCompany" label="Tea Company" />
-        <q-input class="col-10" v-model="teaName" label="Tea Name" />
+      <div class="row">
         <q-input
-          class="col-10"
-          v-model="temperature"
+          class="col-12 q-mb-sm"
+          v-model="teaCompany"
+          label="Tea Company"
+        />
+        <q-input class="col-12 q-mb-sm" v-model="teaName" label="Tea Name" />
+        <q-input class="col-12 q-mb-sm" v-model="leafType" label="Leaf Type" />
+        <q-input
+          class="col-xs-12 col-sm-6"
+          v-model.number="temperature"
           label="Temperature(in Celsius)"
           type="number"
         />
         <q-input
-          class="col-10"
-          v-model="brewTime"
+          class="col-xs-12 col-sm-6"
+          v-model.number="brewTime"
           label="Brew Time(in seconds)"
           type="number"
         />
-      </div>
-      <div>
+        <q-input
+          class="col-12 q-mb-sm"
+          v-model="review"
+          label="Review"
+          type="textarea"
+        />
         <q-btn
           label="Add"
           color="primary"
           icon="add_circle"
           @click="add_onclick"
-          class="col-6"
+          class="q-mt-xl col-xs-12 col-sm-4 col-md-3"
         />
       </div>
     </div>
@@ -34,8 +43,11 @@
 import { ref } from "vue";
 const teaCompany = ref("");
 const teaName = ref("");
+const leafType = ref("");
 const temperature = ref(100);
 const brewTime = ref(120);
+const review = ref("");
+const LEAF_TYPES = ["teabag", "loose leaf"];
 const add_onclick = () => {
   console.log("Add new review");
   console.log("Tea Company: ", teaCompany.value);
